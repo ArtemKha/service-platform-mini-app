c4327df3572968f9f9c03cc1c676feec17f127c0
Artem Khasanov <khasanov@worki.ru>
2021-02-09 17:29:49 +0300

    feat(layout): add form markup and styles

--- src/cljs/service_platform/views.cljs
+++ src/cljs/service_platform/views.cljs
@@ -1,7 +1,42 @@
 (ns service-platform.views
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [reagent.core :as reagent]))
+
+(defn list-item [label]
+  [:div label])
+
+(defn form-item [label]
+  (let [is-active (reagent/atom false)]
+    (fn [lable]
+      [:div
+       [:label {:for label
+                :class (if @is-active "active" "")} label]
+       [:input {:id label :type "text"
+                :on-focus (fn []
+                            (swap! is-active (fn [] true)))
+                :on-blur (fn []
+                           (swap! is-active (fn [] false)))}]])))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
     (fn []
-      [:div "Hello from " @name])))
+      [:div {:class "background"}
+       [:div {:class "wrap"}
+        [:h1 "Service Platform"]
+        [:h3 "Your way to create tasks"]
+        [form-item "Title"]
+        [form-item "Description"]
+        [form-item "Assignee"]
+        [form-item "Applicant"]
+        [form-item "Date"]
+        [:button "Submit"]]])))
+
+(defn list-panel []
+  (let [name (re-frame/subscribe [:name])]
+    (fn []
+      [:div {:class "background"}
+       [:div {:class "wrap"}
+        [:h1 "Service Platform"]
+        [:h3 "Your way to create tasks"]
+        (list-item "Title")
+        [:button "Create application"]]])))
